3970d84c0f98038a8dc091a437ecbcdab9b5d0ec
Jônatas <jonatasthome04@gmail.com>
2025-03-07 17:34:51 -0300

    Project created

---
+++ src/main/java/com/devsuperior20/dscommerce/DscommerceApplication.java
@@ -0,0 +1,15 @@
+package com.devsuperior20.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+
+	
+
+}
---
+++ src/test/java/com/devsuperior20/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior20.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
