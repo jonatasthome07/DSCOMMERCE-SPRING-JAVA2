78f5a98c0bc638ffa138bc79f607a3e7d3d00d8c
Jônatas <jonatasthome04@gmail.com>
2025-03-08 09:54:56 -0300

    Payment implementation

--- src/main/java/com/devsuperior20/dscommerce/entities/Payment.java
+++ src/main/java/com/devsuperior20/dscommerce/entities/Payment.java
@@ -1,21 +1,20 @@
 package com.devsuperior20.dscommerce.entities;
+
 import java.time.Instant;
 import java.util.Objects;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table (name = "tb_order")
-public class Order {
+@Table (name = "tb_payment")
+public class Payment {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,70 +23,58 @@ public class Order {
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
 	
-	private OrderStatus status;
+	@OneToOne
+	@MapsId
+	private Order order;
 	
-	@ManyToOne
-	@JoinColumn(name = "client_id")
-	private User client;
 	
-	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	private Payment payment;
-	
-	public Order() {
+	public Payment() {
 		}
 
-	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+
+	public Payment(Long id, Instant moment, Order order) {
+		super();
 		this.id = id;
 		this.moment = moment;
-		this.status = status;
-		this.client = client;
-		this.payment = payment;
+		this.order = order;
 	}
+
 
 	public Long getId() {
 		return id;
 	}
 
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 
 	public Instant getMoment() {
 		return moment;
 	}
 
+
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
 
-	public OrderStatus getStatus() {
-		return status;
+
+	public Order getOrder() {
+		return order;
 	}
 
-	public void setStatus(OrderStatus status) {
-		this.status = status;
+
+	public void setOrder(Order order) {
+		this.order = order;
 	}
 
-	public User getClient() {
-		return client;
-	}
-
-	public void setClient(User client) {
-		this.client = client;
-	}
-
-	public Payment getPayment() {
-		return payment;
-	}
-
-	public void setPayment(Payment payment) {
-		this.payment = payment;
-	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -97,11 +84,9 @@ public class Order {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Order other = (Order) obj;
+		Payment other = (Payment) obj;
 		return Objects.equals(id, other.id);
 	}
 	
 	
-
-
 }
