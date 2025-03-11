c95f8264f34b08a3fa91da1a340a0be52f9e355e
Jônatas <jonatasthome04@gmail.com>
2025-03-11 16:45:22 -0300

    FindAll method using pageable

--- src/main/java/com/devsuperior20/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior20/dscommerce/services/ProductService.java
@@ -1,8 +1,12 @@
 package com.devsuperior20.dscommerce.services;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.devsuperior20.dscommerce.dto.ProductDTO;
 import com.devsuperior20.dscommerce.entities.Product;
 import com.devsuperior20.dscommerce.repositories.ProductRepository;
@@ -19,5 +23,10 @@ public class ProductService {
 		Product p = result.get();
 		ProductDTO dto = new ProductDTO(p);
 		return dto;
+	}
+	
+	public Page <ProductDTO> findAll (Pageable pageable) {
+		Page <Product> result = repository.findAll(pageable);
+		return result.map(x -> new ProductDTO(x));
 	}
 }
