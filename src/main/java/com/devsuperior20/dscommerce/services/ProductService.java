261218eea11086a2f9fbe989b36850c1adc148ea
Jônatas <jonatasthome04@gmail.com>
2025-03-12 14:07:05 -0300

    Delete product

--- src/main/java/com/devsuperior20/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior20/dscommerce/services/ProductService.java
@@ -46,7 +46,12 @@ public class ProductService {
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 	}
-
+	
+	@Transactional
+	public void delete (Long id) {
+		repository.deleteById(id);
+	}
+	
 	private void copyDTOtoEntity(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
 		entity.setDescription(dto.getDescription());
